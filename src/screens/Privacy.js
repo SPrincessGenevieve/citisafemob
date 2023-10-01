0ae29e7ffcb5d0c948064905525cba84bf058d51
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-10-01 19:01:05 +0800

    Added ScrollView

--- src/screens/Privacy.js
+++ src/screens/Privacy.js
@@ -3,6 +3,7 @@ import { Text } from "react-native";
 import { View } from "react-native";
 import ConstInput from "../components/ConstInputVisible";
 import ConstButton from "../components/ConstButton";
+import KeyboardWithoutWrapper from "../components/KeyboardWithoutWrapper";
 
 function Privacy(props) {
   const [display, setDisplay] = useState(false);
@@ -11,36 +12,45 @@ function Privacy(props) {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <View style={{ marginBottom: 40 }}>
-        {display ? (
-          <>
-            <Text>Your password has been updated</Text>
-          </>
-        ) : null}
+    <KeyboardWithoutWrapper>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 150,
+        }}
+      >
+        <View style={{ marginBottom: 40 }}>
+          {display ? (
+            <>
+              <Text>Your password has been updated</Text>
+            </>
+          ) : null}
+        </View>
+        <View style={{ width: 330 }}>
+          <ConstInput
+            text={"Current Password"}
+            placeholder="Current Password"
+          ></ConstInput>
+          <ConstInput
+            text={"New Password"}
+            placeholder="New Password"
+          ></ConstInput>
+          <ConstInput
+            text={"Confirm New Password"}
+            placeholder="Confirm New Password"
+          ></ConstInput>
+        </View>
+        <View style={{ width: "60%", marginTop: 50 }}>
+          <ConstButton
+            onPress={updated}
+            height={50}
+            title={"Save Changes"}
+          ></ConstButton>
+        </View>
       </View>
-      <View style={{ width: 330 }}>
-        <ConstInput
-          text={"Current Password"}
-          placeholder="Current Password"
-        ></ConstInput>
-        <ConstInput
-          text={"New Password"}
-          placeholder="New Password"
-        ></ConstInput>
-        <ConstInput
-          text={"Confirm New Password"}
-          placeholder="Confirm New Password"
-        ></ConstInput>
-      </View>
-      <View style={{ width: "60%", marginTop: 50 }}>
-        <ConstButton
-          onPress={updated}
-          height={50}
-          title={"Save Changes"}
-        ></ConstButton>
-      </View>
-    </View>
+    </KeyboardWithoutWrapper>
   );
 }
 
